8f1ecb0f2d3e6b3a08008eac1b638ca3c9876f93
Jesse Pena <jpchato@gmail.com>
2020-02-04 15:11:54 -0800

    javascript lab draft

---
+++ app.js
@@ -0,0 +1,80 @@
+'use strict'
+
+// var visitor = prompt('What is your name?');
+// alert('Welcome ' + visitor + '. I hope you enjoy my webpage.');
+
+// var name = prompt('Is my last name Pena?');
+// var name_ = name.toLowerCase();
+// if (name_ === 'yes'){
+//   console.log(name_ + ', my last name is pena ');
+//   alert('Yes, my last name is Pena'); 
+// } else if (name_ === 'no'){
+//   console.log(' Incorrect, Pena is my last name');
+//   alert('Incorrect, my last name is Pena')
+// }
+// else{
+//   console.log('yes or no answers only');
+//   alert('Yes or no answers only');
+// }
+
+// var school = prompt('Did I graduate from UC Santa Cruz?');
+// var school_ = school.toLowerCase();
+// if (school_ === 'yes'){
+//   console.log('I attended but did not graduate from UC Santa Cruz');
+//   alert('I attended but did not graduate from UC Santa Cruz');
+// }
+// else if (school_ === 'no'){
+//   console.log('Correct, I graduated from The Evergreen State College');
+//   alert('Correct, I graduated from The Evergreen State College');
+// }
+// else {
+//   console.log('yes or no answers only');
+//   alert('yes or no answers only')
+// }
+
+// var service = prompt('Did I serve in the Army?');
+// var service_ = service.toLowerCase();
+// if (service_ === 'yes'){
+//   console.log('Yes, I served in the Army');
+//   alert('Yes, I served in the Army');
+// }
+// else if (service_ === 'no'){
+//   console.log('Incorrect, I served 4 years in the Army');
+//   alert('Incorrect, I served 4 years in the Army');
+// }
+// else {
+//   console.log('yes or no answers only');
+//   alert('yes or no answers only');
+// }
+
+// var work = prompt('Did I work as the Hamburgler at kids parties?');
+// var work_ = work.toLowerCase();
+// if (work_ === 'yes'){
+//   console.log('I didn\'t but it sounds fun');
+//   alert('I didn\'t but it sounds fun');
+// }
+// else if (work === 'no'){
+//   console.log('Correct, I worked as a utility forester');
+//   alert('Correct, I worked as a utility forester');
+// }
+// else {
+//     console.log('yes or no answers only');
+//     alert('yes or no answers only');
+//   }
+
+// var goal = prompt('Am I at CodeFellows to party?');
+// var goal_ = goal.toLowerCase();
+// if (goal_ === 'yes'){
+//   console.log('I am not here to party, I\'m trying to get paid');
+//   alert('I am not here to party, I\'m trying to get paid');
+// }
+// else if (goal_ === 'no'){
+//   console.log('Correct, I\'m here to learn how to code so I can build a better life');
+//   alert('Correct, I\'m here to learn how to code and so I can build a better life')
+// }
+// else{
+//   console.log('yes or no answers only');
+//   alert('yes or no answers only')
+// }
+
+// alert('Goodbye ' + visitor + '. Think of me if you need to hire a software developer')
