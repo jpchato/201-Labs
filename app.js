00cafecdb3ac5af1b82f04b6720009034d309d0d
robbymo81 <robert.l.mosher@gmail.com>
2020-02-06 14:49:28 -0800

    Partner lab changes Rob is driver

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ alert('Welcome ' + visitor + '. I hope you enjoy my webpage.');
 
 var name = prompt('Is my last name Pena?');
 var name_ = name.toLowerCase();
-if (name_ === 'yes'){
+function questions () {
+  
+  if (name_ === 'yes'){
   // console.log(name_ + ', my last name is pena ');
   alert('Yes, my last name is Pena'); 
-  myArr.push(1);
+  //myArr.push(1);
 } else if (name_ === 'no'){
   // console.log(' Incorrect, Pena is my last name');
   alert('Incorrect, my last name is Pena')
@@ -31,7 +33,7 @@ if (school_ === 'yes'){
 else if (school_ === 'no'){
   // console.log('Correct, I graduated from The Evergreen State College');
   alert('Correct, I graduated from The Evergreen State College');
-  myArr.push(1);
+  //myArr.push(1);
 }
 else {
   // console.log('yes or no answers only');
@@ -43,7 +45,7 @@ var service_ = service.toLowerCase();
 if (service_ === 'yes'){
   // console.log('Yes, I served in the Army');
   alert('Yes, I served in the Army');
-  myArr.push(1);
+  //myArr.push(1);
 }
 else if (service_ === 'no'){
   // console.log('Incorrect, I served 4 years in the Army');
@@ -63,7 +65,7 @@ if (work_ === 'yes'){
 else if (work === 'no'){
   // console.log('Correct, I worked as a utility forester');
   alert('Correct, I worked as a utility forester');
-  myArr.push(1);
+  //myArr.push(1);
 }
 else {
     // console.log('yes or no answers only');
@@ -79,16 +81,17 @@ if (goal_ === 'yes'){
 else if (goal_ === 'no'){
   // console.log('Correct, I\'m here to learn how to code so I can build a better life');
   alert('Correct, I\'m here to learn how to code and so I can build a better life');
-  myArr.push(1);
+  //myArr.push(1);
 }
 else{
   // console.log('yes or no answers only');
   alert('yes or no answers only')
 }
+}
+questions();
+// // // Lab 3 Number Guessing Game
 
-// // Lab 3 Number Guessing Game
-
-for (var i = 4; i > 0; i--){
+function guess () {for (var i = 4; i > 0; i--){
   var question = prompt('Guess a number between 1 - 10');
   var answer = '5';
   if (question < '5'){
@@ -102,26 +105,30 @@ for (var i = 4; i > 0; i--){
   if (question == answer){
     alert('You got it right');
     // console.log('You got it right');
-    myArr.push(1);
+    //myArr.push(1);
     break;
   }
 }
+}
+guess ();
   alert('The correct answer is 5')
 
 // // lab3
 
-for (var i=6; i>0; i--){
+function games () {for (var i=6; i>0; i--){
   var nostList = ['Final Fantasy 8', 'Tekken 5', 'Final Fantasy 14', 'Pokemon RBY', 'Final Fantasy 7', 'Marvel vs Capcom 2', 'Street Fighter 4'];
   var nostAnswer = prompt('What is one of my most nostalgic games?');
   if (nostAnswer==nostList[0] || nostAnswer==nostList[1] || nostAnswer==nostList[2] || nostAnswer==nostList[3] || nostAnswer==nostList[4] || nostAnswer==nostList[4] || nostAnswer==nostList[5] || nostAnswer==nostList[5] ||nostAnswer==nostList[6]){
     alert('I love that game!');
     // console.log('I love that game!');
-    myArr.push(1);
+    //myArr.push(1);
     break;
   }
 }
+}
+games ();
 alert('Games that inspire nostalgia: ' + nostList.toString())
-alert('You answered ' + myArr.length + ' correctly')
+//alert('You answered ' + myArr.length + ' correctly')
 // console.log('Games that inspire nostalgia: ' + nostList.toString());
 // console.log('You answered ' + myArr.length + ' correctly');
 
